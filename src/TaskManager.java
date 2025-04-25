88e80cdb73364230e565428892f7dac6f635f13f
Taghreed Ali <taghreed1225@gmail.com>
2025-04-26 00:31:49 +0300

    added remove function

---
+++ src/TaskManager.java
@@ -0,0 +1,18 @@
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+
+public class TaskManager {
+    private List<Task> tasks;
+
+    public TaskManager() {
+        this.tasks = new ArrayList<>();
+    }
+    public boolean removeTask(UUID id) {
+        return tasks.removeIf(task -> task.getId().equals(id));
+    }
+
+}
